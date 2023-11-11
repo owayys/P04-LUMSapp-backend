d4485a8322aac335685ba18a0a96f9913d207687
owayys <owais.ahsan@hotmail.com>
2023-11-11 16:41:19 +0500

    misc updates

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
+// var connection = require('./db/index');
+
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -14,6 +16,10 @@ app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}...`)
+
+    // if (connection.state === 'disconnected') {
+    //     return respond(null, { status: 'fail', message: 'server down' });
+    // }
 })
 
 app.get('/', (req, res) => {
