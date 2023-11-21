c518e142a4d88413e1ce3322e9d8f8c649372912
owayys <owais.ahsan@hotmail.com>
2023-11-21 19:14:10 +0500

    Fixes for cyclic deployment

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import { app } from "./app.js";
 import { config } from "dotenv";
-import { connectDatabse } from "./db/index.js";
+import { connectDatabase } from "./db/index.js";
 import { v2 as cloudinary } from "cloudinary";
 
 config({
-  path: "./config.env",
+    path: "./config.env",
 });
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
-connectDatabse();
+;
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+connectDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+})
