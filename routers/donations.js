191b7b3be6193f3c07e1b9efadb9a76ebf5960c9
Nauman <24100311@lums.edu.pk>
2024-04-28 04:02:25 +0500

    delete-donation

--- routers/donations.js
+++ routers/donations.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
-import { getDonations, createDonations, getSpecificDonation, updateDonation} from "../controllers/donations.js";
+import { getDonations, createDonations, getSpecificDonation, updateDonation, deleteSpecificDonation} from "../controllers/donations.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post("/get", isAuthenticated, getDonations);
 router.post("/create", isAuthenticated, createDonations);
 router.post("/getSpecificDonation", isAuthenticated, getSpecificDonation);
 router.post("/update", isAuthenticated, updateDonation);
+router.post("/deleteSpecificDonation", isAuthenticated, deleteSpecificDonation);
 
 export default router;
